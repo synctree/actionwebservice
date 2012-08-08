1177f0c72968e037105fde19221783703fbb1272
Masahji Stewart <masahji@synctree.com>
2012-08-07 19:08:34 -0700

    Not sure how this ever worked. This fixes an infinite loop

--- lib/action_web_service/support/class_inheritable_options.rb
+++ lib/action_web_service/support/class_inheritable_options.rb
@@ -3,7 +3,9 @@ class Class # :nodoc:
   def class_inheritable_option(sym, default_value=nil)
    class_attribute sym => default_value
     class_eval <<-EOS
-      alias :old_#{sym} :#{sym}
+      class << self
+        alias :old_#{sym} :#{sym}
+      end
 
       def self.#{sym}(value=nil)
         if !value.nil?
